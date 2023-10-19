f412d65d2ac364d7caf9e3c50231394d1839ba3f
Jebin_3 <90751436+jebin232@users.noreply.github.com>
2023-10-19 04:56:09 -1200

    main.py

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 from rembg import remove
 from PIL import Image
 import io
+import shutil
+import os
 
-app = Flask(__name)
+app = FastAPI()
 
-@app.route('/remove_background', methods=['POST'])
-def remove_background():
-    try:
-        # Check if an image file was uploaded
-        if 'image' not in request.files:
-            return jsonify({"error": "No image file provided"}), 400
+# Directory to save temporary image files
+output_dir = 'output_images/'
 
-        image_file = request.files['image']
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-        # Check if the file is allowed
-        if image_file.filename == '':
-            return jsonify({"error": "No selected file"}), 400
+@app.post("/remove_bg/")
+async def remove_background(file: UploadFile):
+    # Save the uploaded image temporarily
+    image_path = os.path.join(output_dir, file.filename)
+    with open(image_path, "wb") as img_file:
+        shutil.copyfileobj(file.file, img_file)
 
-        # Read the uploaded image using PIL
-        input_image = Image.open(image_file)
-        
-        # Removing the background
-        output_image = remove(input_image)
-        
-        # Convert the output image to bytes
-        output_buffer = io.BytesIO()
-        output_image.save(output_buffer, format="PNG")
-        
-        return jsonify({"result": "Background removed successfully", "image": output_buffer.getvalue().decode('latin1')})
+    # Remove the background
+    input_image = Image.open(image_path)
+    output_image = remove(input_image)
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    # Save the output image temporarily
+    output_path = os.path.join(output_dir, f'removed_{file.filename}')
+    output_image.save(output_path, "PNG")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return FileResponse(output_path, media_type="image/png")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
