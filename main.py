b7b7f1cf280e1a4185a96776f448c47b7f5db3ea
Jebin_M <90751436+jebin232@users.noreply.github.com>
2024-10-11 15:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def change_background():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT not set
+    port = int(os.environ.get("PORT", 8000))  # Default to 10000 if PORT not set
     app.run(host="0.0.0.0", port=port)
 
 
